946d66a8ccc2aa9b22de54173b10773ae637b1ce
Andrew <barredknight@gmail.com>
2015-11-02 00:16:01 +0300

    First part - deals with config-file - ok

---
+++ src/CLI.java
@@ -0,0 +1,58 @@
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import root.MyConfig;
+import root.utils.ConfigCarer;
+import root.utils.TreeHelper;
+import view.Preconfigurator;
+
+import java.io.File;
+import java.security.CodeSource;
+
+/**
+ * Created by andrew on 11/1/15.
+ */
+public class CLI extends Application{
+    MyConfig config = new MyConfig(null, null, null, null, null);
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        CodeSource codeSource = CLI.class.getProtectionDomain().getCodeSource();
+        File jarFile = new File(codeSource.getLocation().toURI().getPath());
+        String jarDir = jarFile.getParentFile().getPath();
+        System.out.println(jarDir);
+        config = ConfigCarer.loadConf();    //
+        if (!Preconfigurator.lazyCheck(config)) {
+            Preconfigurator.preconfigure(config);
+            ConfigCarer.saveConf(config);
+        }
+
+        showMainStage(primaryStage);
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public static void showMainStage(Stage primaryStage){
+        TreeView<Button> treeView = TreeHelper.getButtonTree("/home/andrew/ForTests", "new-repository-1");
+
+        SplitPane layout = new SplitPane();
+
+        TabPane tabPane = new TabPane(new Tab("First"), new Tab("Second"));
+        tabPane.getTabs().get(0).setContent(treeView);
+
+        // and another
+        layout.setOrientation(Orientation.HORIZONTAL);
+        HBox hBox = new HBox(new Button("New local"), new Button("Open local"));
+        layout.getItems().addAll(tabPane, new VBox(hBox, new Button("Upload"), new Label("here")));
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
